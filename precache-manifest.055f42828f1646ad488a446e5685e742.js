0f8c1f6c677e0464296edd713fd47ecacb68564f
Muhammad Ibragimov <mibragimov@hotmail.com>
2020-02-08 15:43:59 +0500

    Updates

---
+++ precache-manifest.055f42828f1646ad488a446e5685e742.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30863ddcaebd413ad071ceb7e8443844",
+    "url": "/dashboard/index.html"
+  },
+  {
+    "revision": "18418e12330ea18e3512",
+    "url": "/dashboard/static/css/main.44f92fe6.chunk.css"
+  },
+  {
+    "revision": "ba451d2b446696c2e853",
+    "url": "/dashboard/static/js/2.39e2e313.chunk.js"
+  },
+  {
+    "revision": "702d1f48626f7eca1d5471b408f938c4",
+    "url": "/dashboard/static/js/2.39e2e313.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18418e12330ea18e3512",
+    "url": "/dashboard/static/js/main.4c9a32b0.chunk.js"
+  },
+  {
+    "revision": "fdd095408680e681609b",
+    "url": "/dashboard/static/js/runtime-main.75375fc5.js"
+  },
+  {
+    "revision": "a1b78de6d59fae438b9549ff212e5eca",
+    "url": "/dashboard/static/media/icon-bell.a1b78de6.svg"
+  },
+  {
+    "revision": "6e9bdcd2c3d0974dc83c5074e2f97d75",
+    "url": "/dashboard/static/media/icon-dashboard.6e9bdcd2.svg"
+  },
+  {
+    "revision": "a7fdf16a451bac72c28ac797e2876b4f",
+    "url": "/dashboard/static/media/icon-members.a7fdf16a.svg"
+  },
+  {
+    "revision": "3df5f6f4c93a9944d6c03cf5bf586a6c",
+    "url": "/dashboard/static/media/icon-settings.3df5f6f4.svg"
+  },
+  {
+    "revision": "9da17580cca235ecd3f8a54de7ed346e",
+    "url": "/dashboard/static/media/icon-visits.9da17580.svg"
+  }
+]);
